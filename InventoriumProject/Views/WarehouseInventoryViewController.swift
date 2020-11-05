b1166ec4960df9f41675255fb9856b85d3daf304
Franky Roq <frankyroq@Frankys-iMac.lan>
2020-11-04 22:36:40 -0800

    added input inventory screen

---
+++ InventoriumProject/Views/WarehouseInventoryViewController.swift
@@ -0,0 +1,30 @@
+//
+//  WarehouseInventoryViewController.swift
+//  InventoriumProject
+//
+//  Created by Franky Roq on 11/4/20.
+//  Copyright © 2020 CSUCI_350. All rights reserved.
+//
+
+import UIKit
+
+class WarehouseInventoryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
